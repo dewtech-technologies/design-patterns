fe2e814a84da8a5d5b821a4f918286e674a173f9
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2024-02-03 16:10:33 -0300

    ajuste path rota

--- application/routes/api.php
+++ application/routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/pagar', [PagamentoController::class, 'pagar']);
+Route::post('/v1/dewtech/pagar', [PagamentoController::class, 'pagar']);
